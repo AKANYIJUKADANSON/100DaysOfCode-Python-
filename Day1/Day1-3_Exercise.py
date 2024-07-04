e0cf736ec30a0e399ba6414878df45fefe6570f6
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2024-07-04 08:50:00 +0300

    Day1 Variables

---
+++ Day1/Day1-3_Exercise.py
@@ -0,0 +1,4 @@
+# Request a user to enter their name and 
+# Printing the number of characters of the name
+
+print(len(input("Enter your name?\n")))
---
+++ Day1/band_name_generator.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Hello, welcome to Band_name_generator")
+#2. Ask the user for the city they grew up in.
+user_city = input('Enter the city you grew up in:\n')
+#3. Ask the user for the name of a pet
+user_pet = input('Enter a pet of choice:\n')
+#4. Combine the name of their city and pet and show them their band name
+print('Your band name could be '+ user_city +' '+ user_pet)
+#5. Make sure the input cursor shows on a new line.

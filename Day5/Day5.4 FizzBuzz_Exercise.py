b4fc96490f1ef057d1769d398f3f6463fafb4043
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2024-07-04 09:39:43 +0300

    FizzBuzz Challenge corrected [Re-structuring the if..elif..]

--- Day5/Day5.4 FizzBuzz_Exercise.py
+++ Day5/Day5.4 FizzBuzz_Exercise.py
@@ -4,6 +4,28 @@
 # If the number is divisible by 5, then instead of printing the number it should print "Buzz".
 # And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
 
+
+
+
+
+
+for num in range(1, 101):
+    if num % 3 == 0:
+        num = "Fizz"
+    elif num % 5 == 0:
+        num = "Buzz"
+    elif num % 3 == 0 and num % 5 == 0:
+        num = "FizzBuzz"
+    print(num)
+    
+# The code above will not cater for the FizzBuzz option because
+    # In the if .. elif.. statement, the first option to be true will be executed, so that means the number divisible by both 3 and 5 wll not be worked on since the sttmt has already obtained the true value/option
+
+# ~~~~~~~~~~~~~~SOLUTION~~~~~~~~~~~
+    # The solution is to re-structure the code/ if..elif.. statements, so that we first test the number that is divisible by both 3 and 5 for FizzBuzz, then go ahead to test for those divisible by 3 for fizz and those divisible by 5 for Buzz
+    
+    
+    
 # 1 to 100 numbers
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
